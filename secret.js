d41fd3d723267c9df2c987346ad2df240b16205a
Philip Stachura <philip.stachura@unoworks.ca>
2016-10-02 09:03:10 -0700

    last save

---
+++ secret.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+    $('img, p, h1, h2, h3, br').click(function() {
+        if(cheatsOn == 8472)
+        {
+            gone(this);
+        }
+    })
+
+});
+// a key map of allowed keys
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+var cheatsOn = 0;
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length)
+      activateCheats();
+  } else
+    konamiCodePosition = 0;
+});
+
+function activateCheats() {
+  document.body.style.backgroundImage = "url('http://www.lovethisgif.com/uploaded_images/91924-Description-This-Is-A-Simple-Layout-Showing-The-Matrix-Code-Moving-....gif')";
+
+   alert("Welcome to the matrix...");
+   cheatsOn = 8472;
+}
+
+function gone(elem)
+{
+    elem.style.display = "none";
+}
