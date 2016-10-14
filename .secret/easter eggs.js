56650c20306bbd08cf1076a489bd20eb1ae1736f
Philip Stachura <philip.stachura@unoworks.ca>
2016-10-14 07:41:22 -0700

    working on new secret

--- .secret/easter eggs.js
+++ .secret/easter eggs.js
@@ -52,9 +52,12 @@ function loop(s) {
 
     if(s.hashCode() == 1720023)
     {
+        var speed = 10;
+        var x;
+        var y;
         var img = document.createElement('img');
         img.src = 'images/undineship.png';
-        img.width = 50;
+        img.width = 100;
         document.body.appendChild(img);
         var snowball = document.getElementById("snowballAppear");
         img.style.display = 'block';
@@ -63,7 +66,92 @@ function loop(s) {
         img.style.left = 25 + $(window).scrollLeft() + 'px';
         img.style.top = 25 + $(window).scrollTop() + 'px';
 
+        document.body.onmousemove = function(e) {
+            x = e.clientX;
+            y = e.clientY;
+        };
 
+        var id = setInterval(frame, 1);
+        var prevTime;
+
+        function frame()
+        {
+            if(prevTime == null)
+            {
+                prevTime = new Date();
+                prevTime = prevTime.getTime();
+            }
+            var speed = 5;
+            var xMove = 0.0;
+            var yMove = 0.0;
+            var xDistance = x - trimString(img.style.left) -50;
+            var yDistance = y - trimString(img.style.top) -100;
+            var xRatio = (100/(xDistance+yDistance))*xDistance;
+            var yRatio = (100/(yDistance+xDistance))*yDistance;
+
+            if(xDistance > 0)
+            {
+                var d = new Date();
+                xMove = ((((d.getTime() - prevTime)/1000) * speed) * xRatio) +  parseInt(trimString(img.style.left), 10);
+                if(xMove > x)
+                {
+                    xMove = x;
+                }
+            }
+            else if(xDistance < 0)
+            {
+                var d = new Date();
+
+                xMove = ((((d.getTime() - prevTime)/1000) * speed) * -xRatio) + parseInt(trimString(img.style.left),10);
+                if(xMove < x)
+                {
+                    xMove = x;
+                }
+
+            }
+            else
+            {
+                xMove = x;
+            }
+
+            if(yDistance > 0)
+            {
+                var d = new Date();
+
+                yMove = ((((d.getTime() - prevTime)/1000) * speed) * yRatio) +  parseInt(trimString(img.style.top), 10);
+                if(yMove > y)
+                {
+                    yMove = y;
+                }
+            }
+            else if(yDistance < 0)
+            {
+                var d = new Date();
+
+                yMove = ((((d.getTime() - prevTime)/1000) * speed) * -yRatio) +  parseInt(trimString(img.style.top), 10);
+                if(yMove < y)
+                {
+                    yMove = y;
+                }
+
+            }
+            else
+            {
+                yMove = y;
+            }
+
+            img.style.left =  xMove + $(window).scrollLeft() +  'px';
+            img.style.top =  yMove  + $(window).scrollTop() +  'px';
+
+            prevTime = new Date();
+            prevTime = prevTime.getTime();
+
+        }
+
+        function trimString(s)
+        {
+            return s.substring(0, s.length - 2)
+        }
     }
 
 
