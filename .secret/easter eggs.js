81ab0f69b8e762f49914725ce821239794ed6ada
Philip Stachura <philip.stachura@unoworks.ca>
2016-10-15 10:10:43 -0700

    secret movement fixed working on shooting effects

--- .secret/easter eggs.js
+++ .secret/easter eggs.js
@@ -52,87 +52,164 @@ function loop(s) {
 
     if(s.hashCode() == 1720023)
     {
-        var speed = 10;
+
+        var mouseDown = 0;
+        document.body.onmousedown = function() {
+          ++mouseDown;
+        }
+        document.body.onmouseup = function() {
+          --mouseDown;
+        }
         var x;
         var y;
         var img = document.createElement('img');
         img.src = 'images/undineship.png';
         img.width = 200;
         document.body.appendChild(img);
-        var snowball = document.getElementById("snowballAppear");
         img.style.display = 'block';
         img.style.position = 'absolute';
+        img.style.zIndex = 100;
 
-        img.style.left = 25 + $(window).scrollLeft() + 'px';
-        img.style.top = 25 + $(window).scrollTop() + 'px';
+        img.style.left = 100 + 'px';
+        img.style.top = 100 + 'px';
+
+
+        var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('width', 100 + "%");
+        svg.setAttribute('height', 100 + "%");
+        svg.style.position = "absolute";
+        svg.id = "svg";
+        svg.style.zIndex = 99;
+        document.body.insertBefore(svg, document.getElementById("firstElement"));
 
         document.body.onmousemove = function(e) {
             x = e.clientX;
             y = e.clientY;
-        };
-
+        }
         var id = setInterval(frame, 5);
         var prevTime;
+        var line;
+        var lineWidth;
+        var step = 0;
 
         function frame()
         {
-            if(prevTime == null)
+
+
+            if(line !== undefined)
             {
-                prevTime = new Date();
-                prevTime = prevTime.getTime();
+                line.remove();
+                lineWidth = 3;
+                step += 1;
+                console.log(step +"");
+                if(step < 500)
+                {
+                    lineWidth = 3;
+                }
+                else if(step < 1000)
+                {
+                    lineWidth = 4;
+                }
+                else if(step < 2500)
+                {
+                    lineWidth = 5;
+                }
+                else if(step < 3000)
+                {
+                    lineWidth = 6;
+                }
+                else if(step < 7000)
+                {
+                    lineWidth = 5;
+                }
+                else if(step < 7250)
+                {
+                    lineWidth = 4;
+                }
+                else if(step < 7500)
+                {
+                    lineWidth = 3;
+                }
+                else if(step < 7750)
+                {
+                    lineWidth = 2;
+                }
+                else if(step < 8000)
+                {
+                    lineWidth = 1;
+                }
+                else if(step < 8500)
+                {
+                    lineWidth = 0;
+                    line.remove();
+                }
+
             }
-            var speed = 5;
-            var xMove = 0.0;
-            var yMove = 0.0;
+
+            var d = new Date();
+
+            var speed = 3;
+            var xMove;
+            var yMove;
             var xNeg;
             var yNeg;
-            var xDistance = (x + $(window).scrollLeft()) - parseInt(trimString(img.style.left), 10) - 100 ;
-            var yDistance = (y + $(window).scrollTop()) - parseInt(trimString(img.style.top), 10) - 100 ;
-            if(xDistance > 0)
-            {
-                xNeg = 1;
-            }
-            else
-            {
-                xNeg = -1;
-            }
-           if(yDistance > 0)
-            {
-                yNeg = 1;
-            }
-            else
-            {
-                yNeg = -1;
-            }
+            var xDistance = (x + $(window).scrollLeft()) - parseFloat(trimString(img.style.left), 10) - 100 ;
+            var yDistance = (y + $(window).scrollTop()) - parseFloat(trimString(img.style.top), 10) - 100 ;
+            xNeg = (xDistance < 0 ) ? -1 : 1;
+            yNeg = (yDistance < 0 ) ? -1 : 1;
             xDistance = Math.abs(xDistance);
             yDistance = Math.abs(yDistance);
             var xRatio = (100/(xDistance+yDistance))*xDistance;
             var yRatio = (100/(yDistance+xDistance))*yDistance;
 
-            var angle = Math.atan2((y + $(window).scrollTop()) - (parseInt(trimString(img.style.top)) + 100), (x + $(window).scrollLeft()) - (parseInt(trimString(img.style.left)) + 100));
+            var angle = Math.atan2((y + $(window).scrollTop()) - (parseFloat(trimString(img.style.top)) + 100), (x + $(window).scrollLeft()) - (parseFloat(trimString(img.style.left)) + 100));
             angle = angle * (180/Math.PI) + 90;
 
-            var d = new Date();
-            if(!(parseInt(trimString(img.style.left), 10) + 100 == (x + $(window).scrollLeft()) && parseInt(trimString(img.style.top), 10) + 100 ==  (y + $(window).scrollTop())))
+
+            if(!(parseFloat(trimString(img.style.left), 10) + 100 == (x + $(window).scrollLeft()) && parseFloat(trimString(img.style.top), 10) + 100 ==  (y + $(window).scrollTop())))
             {
                 img.style.transform = "rotate(" + angle + "deg)";
             }
 
-            console.log(xNeg + " : " + yNeg);
+            if(mouseDown && step < 8500)
+            {
+                var xClick = x;
+                var yClick = y;
+                line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+                line.setAttribute('x1', parseFloat(trimString(img.style.left), 10) + 100);
+                line.setAttribute('y1', parseFloat(trimString(img.style.top), 10) + 100);
+                line.setAttribute('x2', xClick + $(window).scrollLeft());
+                line.setAttribute('y2', yClick + $(window).scrollTop());
+                line.setAttribute('stroke', "rgb(255,210,30)");
+                line.setAttribute('stroke-width', lineWidth);
+                document.getElementById("svg").appendChild(line);
 
-            img.style.left =  parseInt(trimString(img.style.left), 10) + ((((d.getTime() - prevTime)/1000) * speed) * xRatio) * xNeg  + 'px';
-            img.style.top =  parseInt(trimString(img.style.top), 10) + ((((d.getTime() - prevTime)/1000) * speed) * yRatio) * yNeg + 'px';
+            }
+            else if (mouseDown == 0)
+            {
+                step = 0;
 
+                xMove = parseFloat(trimString(img.style.left), 10) + (((((d.getTime() - prevTime)/1000.0) * speed) * xRatio) * xNeg);
+                yMove = parseFloat(trimString(img.style.top), 10) + (((((d.getTime() - prevTime)/1000.0) * speed) * yRatio) * yNeg);
+
+
+                img.style.left = xMove + 'px'
+                img.style.top = yMove + 'px'
+
+            }
 
             prevTime = new Date();
             prevTime = prevTime.getTime();
 
+
+            function trimString(s)
+            {
+                return s.substring(0, s.length - 2)
+            }
+
+
         }
 
-        function trimString(s)
-        {
-            return s.substring(0, s.length - 2)
-        }
     }
 
 
@@ -147,7 +224,6 @@ document.onclick = function() {
         document.body.appendChild(img);
         var x = event.clientX;
         var y = event.clientY;
-        var snowball = document.getElementById("snowballAppear");
         img.style.display = 'block';
         img.style.position = 'absolute';
         img.style.left = x - 25 + $(window).scrollLeft() + 'px';
@@ -165,3 +241,4 @@ String.prototype.hashCode = function() {
   }
   return hash;
 };
+
