c2796ed8dd7cbd77249112a725168d3b73e070eb
Philip Stachura <philip.stachura@unoworks.ca>
2016-10-20 09:27:36 -0700

    changed radials calculations

--- radials.js
+++ radials.js
@@ -1,20 +1,13 @@
 function placeInRadial(img, rotationOffset)
 {
 
-    var r = convertToRotation(0 + rotationOffset - 90);
+    var r = rotationOffset - 90;
+    var coords = radialPoint(r , {x:169.5, y:90}, 110);
+    img.style.left = coords.x + 'px';
+    img.style.top = coords.y + 'px';
 
-   var id = setInterval(frame, 100);
-   function frame(){
-        var coords = radialPoint(r , {x:169.5, y:90}, 110);
-        img.style.left = coords.x + 'px';
-        img.style.top = coords.y + 'px';
-       // r += convertToRotation(10);
-    }
+    img.distance = 110;
 
-    function convertToRotation(r)
-    {
-        return r/180*Math.PI;
-    }
 }
 
 
@@ -24,11 +17,11 @@ function radialPoint(rotation, center, distance)
         y:calculateY(rotation, center.y, distance)};
     function calculateX(rotation, center, distance)
     {
-         return center + Math.cos(rotation)*distance;
+         return center + Math.cos(rotation/180*Math.PI)*distance;
     }
     function calculateY(rotation, center, distance)
     {
-        return center + Math.sin(rotation)*distance;
+        return center + Math.sin(rotation/180*Math.PI)*distance;
     }
 }
 
