f7b11357454c18a9e1fa0afe7d93d5d279feeae0
Dr-Clutchmaster <shamiwilliam@gmail.com>
2016-11-01 09:21:57 -0700

    Add files via upload
    DoneDoneDone

--- test.atom.js
+++ test.atom.js
@@ -1,11 +1,11 @@
-function function1() {
+function trickQuestion() {
     document.getElementById("p1").innerHTML = "Have you seen it yet?";
     document.getElementById("button2").style.display = "block";
     document.getElementById("button3").style.display = "block";
     /*activates Buttons Yes and No*/
 }
 
-function function2() {
+function yes() {
     document.getElementById("p1").innerHTML = "Then you must be ready for a challenge.";
     document.getElementById("h1").innerHTML = "TEST YOUR CLICK SPEED!";
     document.getElementById("p0").innerHTML = "With this game you can test your averge click speed in clicks per second.";
@@ -15,7 +15,7 @@ function function2() {
     /*activates the game*/
 }
 
-function function3() {
+function no() {
     document.getElementById("p1").innerHTML = "Then you must be blind.";
     document.getElementById("button2").style.display = "none";
     document.getElementById("button3").style.display = "none";
@@ -25,38 +25,42 @@ function function3() {
   }
     /*back to Original Page*/
 }
-
+var goalScore = 100;
 var score = 0;
 var startTime;
 var timerStarted = false;
 
-function function4() {
-    document.getElementById("button4").innerHTML = "your score is: " + score;
+function theGame() {
   if (score < 1) {
     timerStarted = true;
     var date = new Date();
-    date = date.getDate/1000;
+    date = date.getTime()/100;
     startTime = date;
+    ad();
    }
   if (timerStarted) {
     score++;
-    if (score > 5) {
+    document.getElementById("button4").innerHTML = "your score is: " + score;
+    if (score >= goalScore) {
       timerStarted = false;
       var date = new Date();
-      document.getElementById("p2").innerHTML = date.getTime/1000;
-      //document.getElementById("p2").innerHTML = (startTime-date.getTime)+"per 100 clicks";
+      document.getElementById("p2").innerHTML =  "10 Clicks per " + (date.getTime()/100 - startTime)/goalScore + "s";
     }
+
   }
+
+
+
       /*the game itself*/
 }
 
-function function5() {
+function back2Start() {
   document.getElementById("p1").innerHTML = "Btw look what I can do";
   document.getElementById("button5").style.display = "none";
   /*ok Button*/
 }
 
-function function6() {
+function ad() {
   document.getElementById("p3").style.display = "block";
   /*advertises philip.stachura.me*/
 }
